9bf4fc81928e8e89e59ef49f6c814d0068edb647
Preslav Rachev <rachev.preslav@gmail.com>
2017-10-29 16:40:32 +0100

    Creates an initial commit
    - Ignore rules thanks to GitHub: https://github.com/github/gitignore/blob/master/Swift.gitignore

--- CryptoFolio/AppDelegate.swift
+++ CryptoFolio/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Preslav Rachev. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import CoreData
 
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
 
