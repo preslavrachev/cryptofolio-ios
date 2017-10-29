bd765d69756fc8ff04d617efc1a1b5426f506694
Preslav Rachev <rachev.preslav@gmail.com>
2017-10-29 08:58:32 +0100

    Initial Commit

---
+++ CryptoFolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoFolio
+//
+//  Created by Preslav Rachev on 29.10.17.
+//  Copyright © 2017 Preslav Rachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
